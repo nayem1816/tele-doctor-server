8a055ae95183199b3b180c9131e6cce747492736
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-08-31 20:34:05 +0600

    add profile model, controler and create post method createProfile

---
+++ src/models/ProfileModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const DataSchema = mongoose.Schema(
+    {
+        Name: { type: String, required: true },
+        EmailAddress: { type: String, required: true },
+        MobileNumber: { type: String, min: 11, max: 14, default: null },
+        ProfilePic: { type: String, default: null },
+        Address: { type: String, default: null },
+        DateOfBirth: { type: String, default: null },
+        Gender: { type: String, default: null },
+    },
+    { versionKey: false }
+);
+const ProfileModel = mongoose.model('profiles', DataSchema);
+module.exports = ProfileModel;
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const ProfileController = require('../controllers/ProfileController');
 
 const router = express.Router();
+
+router.post('/CreateProfile', ProfileController.CreateProfile);
 
 module.exports = router;
